19472f6109bd7790dde11cff5817b2eb31fc2e1d
lindalbertort7 <lindalberto.rufino@rt7.com.br>
2017-12-07 16:52:47 -0200

    feat: primeiro commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import Auth0Cordova from '@auth0/cordova';
 @Component({
   templateUrl: 'app.html'
 })
@@ -16,6 +17,11 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      // Add this function
+      (<any>window).handleOpenURL = (url) => {
+        Auth0Cordova.onRedirectUri(url);
+      };
     });
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,18 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public auth: AuthService) {
+    //domain: rt7-teste.auth0.com
+    //client-id: g5XUUDzfe6UG2AsGV-ORvUKPYOGBwJcY
+    //client-secret: zx6DxOz7IP8c7KDvX4X_ZBJkqfF0VAeUMki8O7Bi5U14Hfq99UUCDpR_Zy8P8sXt
 
+    //# replace YOUR_PACKAGE_ID with your app package ID
+    //io.ionic.rt7.teste://rt7-teste.auth0.com/cordova/io.ionic.rt7.teste/callback
+
+
+    //# replace YOUR_PACKAGE_ID with your app identifier
+    //cordova plugin add cordova-plugin-customurlscheme --variable URL_SCHEME=io.ionic.rt7.teste --variable ANDROID_SCHEME=io.ionic.rt7.teste --variable ANDROID_HOST=rt7-teste.auth0.com --variable ANDROID_PATHPREFIX=/cordova/io.ionic.rt7.teste/callback
+    console.log(auth.isAuthenticated());
   }
 
 }
